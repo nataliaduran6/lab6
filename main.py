e298513dff619a845acf1c7b8ac5d606654147fb
anili <anagarcia1@ufl.edu>
2022-11-07 00:07:18 -0500

    Added decode

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ def encode(password):
         encoded_pass += digit
     return encoded_pass
 
-def decode(encoded_pass):
-    decoded_pass = ""
-    for i in range(pass_length):
-        digit = str(int(encoded_pass[i])-3)
-        decoded_pass += digit
-    return decoded_pass
+def decode(new_pass):
+    password = []
+    for i in new_pass:
+        password.append(int(i))
+    new = []
+    for num in password:
+        num -= 3
+        new.append(num)
+    old_pass = ''.join(map(str, new))
 
+    return old_pass
+#i added my decode
 while True:
     print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
     oper = int(input("Please enter an option: "))
